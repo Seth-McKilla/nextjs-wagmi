ee6acc7640babea2eeb7fa272b25f6bb44f70f62
Seth McCullough <seth@endevrs.dev>
2022-01-09 10:56:50 -0500

    Update page for connected status

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["prod-metadata.s3.amazonaws.com"],
+    domains: ["prod-metadata.s3.amazonaws.com", "ethereum.org"],
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import { useState } from "react";
 import type { NextPage } from "next";
+import { useAccount, useBalance } from "wagmi";
 import { Button, Layout, WalletOptionsModal } from "../components";
 
 const Home: NextPage = () => {
   const [showWalletOptions, setShowWalletOptions] = useState(false);
+  const [{ data: accountData }] = useAccount();
+  const [{ data: balanceData }] = useBalance({
+    addressOrName: accountData?.address,
+  });
 
   return (
     <>
@@ -16,11 +22,32 @@ const Home: NextPage = () => {
         <div className="grid place-items-center h-screen">
           <div className="grid place-items-center">
             <h1 className="text-4xl font-bold mb-8">
-              Welcome to the NextJS wagmi template!
+              {accountData
+                ? "Account Details"
+                : "Welcome to the NextJS wagmi template!"}
             </h1>
-            <Button onClick={() => setShowWalletOptions(true)}>
-              Connect to Wallet
-            </Button>
+            {accountData && balanceData ? (
+              <>
+                <h6 className="text-2xl mb-2">{`Wallet Address: ${accountData.address}`}</h6>
+                <div className="inline-flex place-items-center">
+                  <Image
+                    src={
+                      "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/cdbe4/eth-diamond-black.webp"
+                    }
+                    alt="ETH Diamond"
+                    width={40}
+                    height={65}
+                  />
+                  <h6 className="text-2xl ml-2">{`${Number(
+                    balanceData.formatted
+                  ).toFixed(4)} Ether`}</h6>
+                </div>
+              </>
+            ) : (
+              <Button onClick={() => setShowWalletOptions(true)}>
+                Connect to Wallet
+              </Button>
+            )}
           </div>
         </div>
       </Layout>
