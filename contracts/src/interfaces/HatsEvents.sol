397ce1281beabf0b992d6241fe6ffb9c88a5f071
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-08-12 16:43:25 +0800

    feat: hats event interface

---
+++ contracts/src/interfaces/HatsEvents.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: AGPL-3.0
+// Copyright (C) 2023 Haberdasher Labs
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >=0.8.13;
+
+interface HatsEvents {
+    /// @notice Emitted when a new hat is created
+    /// @param id The id for the new hat
+    /// @param details A description of the Hat
+    /// @param maxSupply The total instances of the Hat that can be worn at once
+    /// @param eligibility The address that can report on the Hat wearer's status
+    /// @param toggle The address that can deactivate the Hat
+    /// @param mutable_ Whether the hat's properties are changeable after creation
+    /// @param imageURI The image uri for this hat and the fallback for its
+    event HatCreated(
+        uint256 id,
+        string details,
+        uint32 maxSupply,
+        address eligibility,
+        address toggle,
+        bool mutable_,
+        string imageURI
+    );
+
+    /// @notice Emitted when a hat wearer's standing is updated
+    /// @dev Eligibility is excluded since the source of truth for eligibility is the eligibility module and may change without a transaction
+    /// @param hatId The id of the wearer's hat
+    /// @param wearer The wearer's address
+    /// @param wearerStanding Whether the wearer is in good standing for the hat
+    event WearerStandingChanged(uint256 hatId, address wearer, bool wearerStanding);
+
+    /// @notice Emitted when a hat's status is updated
+    /// @param hatId The id of the hat
+    /// @param newStatus Whether the hat is active
+    event HatStatusChanged(uint256 hatId, bool newStatus);
+
+    /// @notice Emitted when a hat's details are updated
+    /// @param hatId The id of the hat
+    /// @param newDetails The updated details
+    event HatDetailsChanged(uint256 hatId, string newDetails);
+
+    /// @notice Emitted when a hat's eligibility module is updated
+    /// @param hatId The id of the hat
+    /// @param newEligibility The updated eligibiliy module
+    event HatEligibilityChanged(uint256 hatId, address newEligibility);
+
+    /// @notice Emitted when a hat's toggle module is updated
+    /// @param hatId The id of the hat
+    /// @param newToggle The updated toggle module
+    event HatToggleChanged(uint256 hatId, address newToggle);
+
+    /// @notice Emitted when a hat's mutability is updated
+    /// @param hatId The id of the hat
+    event HatMutabilityChanged(uint256 hatId);
+
+    /// @notice Emitted when a hat's maximum supply is updated
+    /// @param hatId The id of the hat
+    /// @param newMaxSupply The updated max supply
+    event HatMaxSupplyChanged(uint256 hatId, uint32 newMaxSupply);
+
+    /// @notice Emitted when a hat's image URI is updated
+    /// @param hatId The id of the hat
+    /// @param newImageURI The updated image URI
+    event HatImageURIChanged(uint256 hatId, string newImageURI);
+
+    /// @notice Emitted when a tophat linkage is requested by its admin
+    /// @param domain The domain of the tree tophat to link
+    /// @param newAdmin The tophat's would-be admin in the parent tree
+    event TopHatLinkRequested(uint32 domain, uint256 newAdmin);
+
+    /// @notice Emitted when a tophat is linked to a another tree
+    /// @param domain The domain of the newly-linked tophat
+    /// @param newAdmin The tophat's new admin in the parent tree
+    event TopHatLinked(uint32 domain, uint256 newAdmin);
+}
