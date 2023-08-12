04de613fd7484276150bcc4ea1a9f0963e06fe5f
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-08-12 18:57:33 +0800

    chore: DAOMembershipNFTFactory -> DAOMembershipNFT

--- contracts/src/DAOMembershipNFT.sol
+++ contracts/src/DAOMembershipNFT.sol
@@ -7,11 +7,11 @@ import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/Counters.sol";
 
 /**
- * @title DAOMembershipNFTFactory
+ * @title DAOMembershipNFT
  * @dev This contract allows for the creation of DAO membership NFTs that are soulbound to an address.
  * It uses the ERC-1155 standard, which supports both fungible and non-fungible tokens.
  */
-contract DAOMembershipNFTFactory is ERC1155, Ownable {
+contract DAOMembershipNFT is ERC1155, Ownable {
     using Counters for Counters.Counter; // Using the Counters library for managing token IDs
 
     // Counter for generating unique token IDs
@@ -24,22 +24,46 @@ contract DAOMembershipNFTFactory is ERC1155, Ownable {
      * @dev Constructor that sets the metadata URI for the tokens.
      * The metadata for each token would be fetched from this endpoint by replacing `{id}` with the actual token ID.
      */
-    constructor() ERC1155("https://yourapi.com/api/token/{id}.json") {}
+    constructor(string memory uri) ERC1155(uri) {}
+
+    //Include a way to edit the metadata URI
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
 
     /**
      * @dev Allows the owner (presumably the DAO) to mint new membership NFTs.
      * Each NFT is soulbound to the specified address.
-     * @param account Address to which the NFT will be soulbound.
-     * @param amount Amount of NFTs to mint.
+     * @param account Address to which the NFT will be soulbound. In this case, the address of the new member.
      * @return The ID of the minted NFT.
      */
-    function createMembershipNFT(address account, uint256 amount) external onlyOwner returns (uint256) {
+    function createMembershipNFT(address account) external onlyOwner returns (uint256) {
         _tokenIds.increment(); // Incrementing the token ID counter
         uint256 newTokenId = _tokenIds.current(); // Getting the current token ID
-        _mint(account, newTokenId, amount, ""); // Minting the NFT
+        _mint(account, newTokenId, 1, ""); // Minting the NFT
         _soulboundAddresses[newTokenId] = account; // Setting the soulbound address for the NFT
         return newTokenId;
     }
+
+    /**
+     * @dev Allows the owner (presumably the DAO) to mint new membership NFTs in batch.
+     * Each NFT is soulbound to the respective address in the provided list.
+     * @param accounts Addresses to which the NFTs will be soulbound.
+     * @return An array of IDs for the minted NFTs.
+    */
+function batchCreateMembershipNFT(address[] memory accounts) external onlyOwner returns (uint256[] memory) {
+    uint256[] memory mintedTokenIds = new uint256[](accounts.length);
+
+    for (uint256 i = 0; i < accounts.length; i++) {
+        _tokenIds.increment(); // Incrementing the token ID counter
+        uint256 newTokenId = _tokenIds.current(); // Getting the current token ID
+        _mint(accounts[i], newTokenId, 1, ""); // Minting the NFT
+        _soulboundAddresses[newTokenId] = accounts[i]; // Setting the soulbound address for the NFT
+        mintedTokenIds[i] = newTokenId;
+    }
+    
+    return mintedTokenIds;
+}
 
     /**
      * @dev Checks if a token is soulbound to a specific address.
