f8d8e84f2eaf7c8d05a652b35a822e4f471f0052
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-08-13 20:49:42 +0800

    chore: bump sol version

--- contracts/src/SubDAOPostNFTFactory.sol
+++ contracts/src/SubDAOPostNFTFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.19;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
